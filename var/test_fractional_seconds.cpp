8ecbecb752770154266b6bdbd8c313e424531719
xanthos <xanthos@mail.ntua.gr>
2021-11-09 23:03:40 +0200

    added unit test for fractional seconds

--- var/test_fractional_seconds.cpp
+++ var/test_fractional_seconds.cpp
@@ -1,5 +1,4 @@
 #include "dtfund.hpp"
-#include <iostream>
 #include <stdexcept>
 #include <vector>
 #include <cassert>
@@ -11,24 +10,24 @@ using dso::nanoseconds;
 int main() {
   std::vector<double> fsec = {1e0, 0e0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11, 1e-12};
 
-  printf("%14s %14s %14s %14s\n", "F. Seconds", "From Milli", "From Micro", "From Nano");
+  // printf("%14s %14s %14s %14s\n", "F. Seconds", "From Milli", "From Micro", "From Nano");
   for (const auto fs : fsec) {
     milliseconds mls {static_cast<long>(fs * milliseconds::sec_factor<double>())};
     microseconds mrs {static_cast<long>(fs * microseconds::sec_factor<double>())};
     nanoseconds nns {static_cast<long>(fs * nanoseconds::sec_factor<double>())};
 
-    printf("%.12f %.12f %.12f %.12f %E %E %E\n", fs, mls.to_fractional_seconds(), mrs.to_fractional_seconds(), nns.to_fractional_seconds(), mls.to_fractional_seconds(), mrs.to_fractional_seconds(), nns.to_fractional_seconds());
+    // printf("%.12f %.12f %.12f %.12f %E %E %E\n", fs, mls.to_fractional_seconds(), mrs.to_fractional_seconds(), nns.to_fractional_seconds(), mls.to_fractional_seconds(), mrs.to_fractional_seconds(), nns.to_fractional_seconds());
 
     if (fs >= 1e0/milliseconds::sec_factor<double>())
       assert(mls.to_fractional_seconds() == fs);
     else
       assert(mls.to_fractional_seconds() == 0e0);
-    
+
     if (fs >= 1e0/microseconds::sec_factor<double>())
       assert(mrs.to_fractional_seconds() == fs);
     else
       assert(mrs.to_fractional_seconds() == 0e0);
-    
+
     if (fs >= 1e0/nanoseconds::sec_factor<double>())
       assert(nns.to_fractional_seconds() == fs);
     else
