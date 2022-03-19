8d367f0171dc924c5aec20b55b2d969b2653e1e3
xanthos <xanthos@mail.ntua.gr>
2022-03-19 11:37:53 +0200

    adding time scales

---
+++ src/time_scales.hpp
@@ -0,0 +1,15 @@
+#ifndef __DSO_DATTIME_TIME_SCALES_HPP__
+#define __DSO_DATTIME_TIME_SCALES_HPP__
+
+namespace dso {
+
+/// @brief Time Scales
+enum class TimeScale : char {
+  TAI,
+  UTC,
+  UT1,
+  TT
+}; // TimeScale
+
+}// dso
+#endif
