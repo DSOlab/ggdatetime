47596725a261a5e7a596352bf27e0fdc24c9411d
xanthospap <xanthos.papanikolaou@gmail.com>
2023-12-30 21:24:59 +0200

    format

--- src/tpdate2.hpp
+++ src/tpdate2.hpp
@@ -30,12 +30,12 @@ private:
   using FDOUBLE = /*long*/ double;
   int _mjd;      /** Mjd */
   FDOUBLE _fday; /** fractional part of day */
-  
+
   /** Construct from MJD and fractional days.
    *
    * This is only private and should be used in rare cases. Normal users,
-   * should explicitely cast the second argument to FractionalSeconds or 
-   * FractionalDays to avoid misconceptions (i.e. is the parameters fractional 
+   * should explicitely cast the second argument to FractionalSeconds or
+   * FractionalDays to avoid misconceptions (i.e. is the parameters fractional
    * seconds or fractional days?).
    */
   explicit TwoPartDate2(int mjd, FDOUBLE fday) noexcept
