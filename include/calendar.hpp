0767265b07ccef0f756d34d8981d4ca163f60c6e
xanthos@durruti <xanthos.papanikolaou@gmail.com>
2024-12-18 13:18:29 +0200

    merger master

--- include/calendar.hpp
+++ include/calendar.hpp
@@ -16,13 +16,6 @@ template <typename T> using DatetimeUtc = datetime_utc<T>;
 /** alias for TwoPartDate */
 using MjdEpoch = TwoPartDate;
 
-void foobar() 
-
-{
-  if (1==2) { return 1;}
-  else {return 2;}
-}
-
 } /* namespace dso */
 
 #endif
